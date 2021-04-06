f2dcccfc089ed79674b8965ed7f751378ebd702b
Patrick Kelley <patrickk@23andme.com>
2021-04-06 10:36:56 -0600

    Adding lintly support for terrascan

--- lib/functions/lintly.sh
+++ lib/functions/lintly.sh
@@ -16,6 +16,7 @@ LINTLY_SUPPORT_ARRAY['PYTHON_BLACK']="black"
 LINTLY_SUPPORT_ARRAY['PYTHON_PYLINT']="pylint-json"
 LINTLY_SUPPORT_ARRAY['JAVASCRIPT_ES']="eslint"
 LINTLY_SUPPORT_ARRAY['TYPESCRIPT_ES']="eslint"
+LINTLY_SUPPORT_ARRAY['TERRAFORM_TERRASCAN']="terrascan"
 LINTLY_SUPPORT_ARRAY['CSS']="stylelint"
 LINTLY_SUPPORT_ARRAY['DOCKERFILE_HADOLINT']="hadolint"
 LINTLY_SUPPORT_ARRAY['CLOUDFORMATION']="cfn-lint"
@@ -33,6 +34,7 @@ export LINTLY_SUPPORT_ARRAY                      # Workaround SC2034
 function AddLinterOptsForLintly() {
   [[ ! "${LINTER_OPTS[PYTHON_PYLINT]}" =~ "json" ]] && LINTER_OPTS[PYTHON_PYLINT]+=" --output-format=json"
   [[ ! "${LINTER_OPTS[DOCKERFILE_HADOLINT]}" =~ "json" ]] && LINTER_OPTS[DOCKERFILE_HADOLINT]+=" --format json"
+  [[ ! "${LINTER_OPTS[TERRAFORM_TERRASCAN]}" =~ "json" ]] && LINTER_OPTS[TERRAFORM_TERRASCAN]+=" -o json"
 }
 ################################################################################
 #### Function InvokeLintly #####################################################
